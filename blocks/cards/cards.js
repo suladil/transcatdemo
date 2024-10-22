ecabf62031d75a650d4fdeb3bf48502e5dbb3e01
suladil <119360610+suladil@users.noreply.github.com>
2024-10-22 16:08:49 -0400

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,23 +2,36 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  // Create a list element with a 'cards' class
   const ul = document.createElement('ul');
+  ul.className = 'cards'; // Ensure this class is set for styling
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
+    
     while (row.firstElementChild) li.append(row.firstElementChild);
+    
+    // Assign classes for styling based on child content
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
+    
     ul.append(li);
   });
+
+  // Optimize images
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
+  // Clear the block's content and append the new list
   block.textContent = '';
   block.append(ul);
 }
